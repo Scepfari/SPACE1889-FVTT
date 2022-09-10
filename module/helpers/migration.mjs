c131e2f8cc592acee6f9abf6eb2d961b8d14c3ed
Scepfari <63742921+Scepfari@users.noreply.github.com>
2022-09-10 22:39:20 +0200

    update Versionsinfo
    inkl. englischer Version

--- module/helpers/migration.mjs
+++ module/helpers/migration.mjs
@@ -53,54 +53,23 @@ export class Space1889Migration
 		const currentVersion = game.system.data.version;
 		if (game.user.isGM && (isNewerVersion(currentVersion, info[1]) || info[0] > 0))
 		{
-			let content = `<h1>SPACE 1889 Neocristallicum</h1>
-			<p>&nbsp;</p>
-			<h3>0.7.5</h3>
-			<ul>
-			<li>[STRG] + [SHIFT] + Mausklick auf ein Attributswurf, w&uuml;rfelt den einfachen Attributswert, anstelle vom Doppelten</li>
-			<li>Versionshinweisanzeige</li>
-			<li>Fix Negative Gesundheit: Reduziert jetzt die Beladung und die Bewegung ensprechend GRW S. 224</li>
-			<li>Fix Talent Volle Abwehr: Abwehrbonus auf h&ouml;heren Stufen wird nun korrekt eingerechnet</li>
-			</ul>
-			<p>&nbsp;</p>
-			<h3>0.7.4</h3>
-			<ul>
-			<li>&Uuml;berarbeitung der Beschreibung/Description in den Item Sheets, d.h. die beiden Tabs wurden in einem vereinigt, wobei die Unterscheidung zwischen den &uuml;bersetzbaren Kompendien-Texten (aus der Sprachdatei) und den Nutzer Texten/Anmerkungen erhalten bleibt.</li>
-			<li>Gewicht der Gegenst&auml;nde aus dem Lager wird bei den Charakteren nun mit angezeigt</li>
-			</ul>
-			<p>&nbsp;</p>
-			<h3>0.7.3:</h3>
-			<ul>
-			<li>Vereinheitlichung der W&uuml;rfeltypen, d.h. bei allen Proben wird mit den gleichen W&uuml;rfeln gew&uuml;rfelt, l&auml;sst sich in den Systemeinstellungen einstellen ob mit M&uuml;nzen (dc) oder W&uuml;rfeln (d6) gew&uuml;rfelt werden soll</li>
-			<li>Bugfixes</li>
-			</ul>
-			<p>&nbsp;</p>
-			<h3>0.7.2</h3>
-			<ul>
-			<li>Fix: Kompendien Name &Uuml;bersetzung</li>
-			<li>NSC Kompendien hinzugef&uuml;gt</li>
-			</ul>
-			<p>&nbsp;</p>
-			<h3>0.7.1</h3>
-			<ul>
-			<li>Mehrsprachenunterst&uuml;tzung, einschlie&szlig;lich Umbenennung der Elemente in den Kompendien</li>
-			<li>Englische &Uuml;bersetzung</li>
-			<li>Verbesserte Chatausgabe, insbesondere werden im Normalfall die Beschreibungen nur noch an einen selbst gefl&uuml;stert und nur bei zus&auml;tzlichem Halten von [Strg] oder [Shift] &ouml;ffentlich in den Chat geschoben. Diese Beschreibungsw&uuml;rfe k&ouml;nnen jetzt auch mit Akteuren ausgef&uuml;hrt werden, bei denen der Spieler nur eingeschr&auml;nkte Rechte hat.</li>
-			<li>Bugfixes</li>
-			</ul>`;
+			let content = game.i18n.localize("SPACE1889.VersionInfo0_8_0");
+			const understood = game.i18n.localize("SPACE1889.Understood");
+			const stayAway = game.i18n.localize("SPACE1889.StayAway");
+			const newVersion = game.i18n.localize("SPACE1889.NewVersion");
 
 			new Dialog({
-				title: `neue Version ${currentVersion}`,
+				title: `${newVersion} ${currentVersion}`,
 				content,
 				buttons: {
 					ok: {
 						icon: '<i class="fas fa-check"></i>',
-						label: "Verstanden",
+						label: `${understood}`,
 						callback: () => game.settings.set("space1889", "newVersionPopup", `1|${currentVersion}`),
 					},
 					dont_remind: {
 						icon: '<i class="fas fa-times"></i>',
-						label: "Bleib weg",
+						label: `${stayAway}`,
 						callback: () => game.settings.set("space1889", "newVersionPopup", `0|${currentVersion}`),
 					},
 				},
